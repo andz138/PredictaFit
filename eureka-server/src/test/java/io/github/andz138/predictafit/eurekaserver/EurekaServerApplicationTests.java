cadda8e13cf22504dbbae19fb8ceebff102cad94
asosa <145294065+andz138@users.noreply.github.com>
2026-02-06 23:39:44 -0500

    Add Eureka Server service discovery

---
+++ eureka-server/src/test/java/io/github/andz138/predictafit/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.andz138.predictafit.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
