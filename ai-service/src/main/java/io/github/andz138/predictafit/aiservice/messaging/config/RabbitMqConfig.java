b4d8b289327d7b0548ae753bf8681718d89b82bd
asosa <145294065+andz138@users.noreply.github.com>
2026-02-18 03:14:45 -0500

    configure RabbitMQ consumer

---
+++ ai-service/src/main/java/io/github/andz138/predictafit/aiservice/messaging/config/RabbitMqConfig.java
@@ -0,0 +1,48 @@
+package io.github.andz138.predictafit.aiservice.messaging.config;
+
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMqConfig {
+    @Value("${rabbitmq.queue.name}")
+    private String queueName;
+
+    @Value("${rabbitmq.exchange.name}")
+    private String exchangeName;
+
+    @Value("${rabbitmq.routing.key}")
+    private String routingKey;
+
+    @Bean
+    public Queue activityQueue() {
+        return QueueBuilder
+                .durable(queueName)
+                .build();
+    }
+
+    @Bean
+    public DirectExchange activityExchange() {
+        return ExchangeBuilder
+                .directExchange(exchangeName)
+                .durable(true)
+                .build();
+    }
+
+    @Bean
+    public Binding activityBinding() {
+        return BindingBuilder
+                .bind(activityQueue())
+                .to(activityExchange())
+                .with(routingKey);
+    }
+
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        return new JacksonJsonMessageConverter();
+    }
+}
