dc8db1187cf547ba91733fe28c772723550b7b15
asosa <145294065+andz138@users.noreply.github.com>
2026-03-05 04:02:30 -0500

    Add simple login button

--- predictafit-frontend/src/App.tsx
+++ predictafit-frontend/src/App.tsx
@@ -1,9 +1,13 @@
-function App() {
+import { Button } from "@mui/material"
+import { BrowserRouter as Router } from "react-router-dom"
 
+function App() {
   return (
-    <>
-      
-    </>
+    <Router>
+      <Button variant="contained" color="primary">
+        LOGIN
+      </Button>
+    </Router>
   )
 }
 
